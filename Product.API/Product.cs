1207b90195ba0c5c8e7bbee27d78aeed27dad2a4
Tamaz.Bagdavadze <Tamaz.Bagdavadze@SPLP0137>
2023-04-06 12:20:39 +0400

    add price2

--- Product.API/Product.cs
+++ Product.API/Product.cs
@@ -2,15 +2,17 @@
 {
     public class Product
     {
-        public Product(int id, string name, double price)
+        public Product(int id, string name, double price, double price2)
         {
             Id = id;
             Name = name;
             Price = price;
+            Price2 = price2;
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public double Price2 { get; set; }
     }
 }
